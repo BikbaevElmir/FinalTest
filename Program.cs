0f5a9aa0fe18316faa566c806c4a8888e8024df1
Elmir Bikbaev <integral@nxt.ru>
2024-04-12 20:52:38 +0500

    Добавлена функция создания нового массива с элементами не более 3 символов в Program.cs

--- Program.cs
+++ Program.cs
@@ -5,3 +5,16 @@
     string[] narray = str.Split(" ");
     return narray;
 }
+
+string[] NewArray(string[] array)
+{
+    string str = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            str = str + " " + array[i];
+        }
+    }
+    return str.Split(" ");
+}
